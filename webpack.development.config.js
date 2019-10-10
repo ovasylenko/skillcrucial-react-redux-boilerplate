59b08c1cb30fda79190d45ffda9249270674f4f4
Lev <lev@alaev.fr>
2019-10-10 09:22:15 +0300

    fix 127.0.0.1

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -32,7 +32,7 @@ const config = {
     hot: true,
     contentBase: resolve(__dirname, 'dist/assets'),
     watchContentBase: true,
-    host: '127.0.0.0',
+    host: '127.0.0.1',
     port: 8088,
 
     historyApiFallback: true,
