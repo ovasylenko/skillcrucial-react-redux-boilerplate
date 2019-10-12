a3d7bc71cc022936bf36cc7617f23ad73131f003
Petr Lebedev <lebedevpetr@outlook.com>
2019-10-12 10:10:24 +0300

    Update webpack.development.config.js

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -32,7 +32,7 @@ const config = {
     hot: true,
     contentBase: resolve(__dirname, 'dist/assets'),
     watchContentBase: true,
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: 3001,
 
     historyApiFallback: true,
