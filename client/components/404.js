6f4e80007ff8409eda56e77a9e443143e48125d9
MrTolerant <lebedevpetr@outlook.com>
2019-11-30 13:00:03 +0300

    404

--- client/components/404.js
+++ client/components/404.js
@@ -6,9 +6,14 @@ import { push } from 'connected-react-router'
 import { connect } from 'react-redux'
 
 const NotFound = (props) => (
-  <div>
-    <h1> 404: URL Is not found </h1>
-    <button type="button" tabIndex="0" onClick={props.goRoot}>Go to root</button>
+  <div className="container main-wrapper aligner">
+    <div className="aligner-item text-center ">
+      <h1 className="display-1">404</h1>
+      <p className="lead text-gray-800 mb-5">Page Not Found</p>
+      <p className="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+      <br />
+      <button className="btn btn-secondary btn-lg" type="button" tabIndex="0" onClick={props.goRoot}> Back to Dashboard</button>
+    </div>
   </div>
 );
 
