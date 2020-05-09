092316be85874c7464d4d87d0fb405f0ebeca4a0
Marina Drozd <patamga@mail.ru>
2020-05-10 02:17:37 +0300

    commit client

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
     <div>
       <Head title="Hello" />
       <div id="title">Dashboard</div>
-      <Link to="/dashboard/profile/fb1209b1-fabc-4e52-bd8e-a9bf47ba392"> Go To Profile </Link>
+      <Link to="/dashboard/profile/fb1209b1-fabc-4e52-bd8e-a9bf47ba392a"> Go To Profile </Link>
       <Link to="/dashboard/main"> Go To Main </Link>
     </div>
   )
--- client/components/main.js
+++ client/components/main.js
@@ -8,7 +8,7 @@ const Main = () => {
     <div>
       <Head title="Hello" />
       <div id="title">Main</div>
-      <Link to="/dashboard/profile/fb1209b1-fabc-4e52-bd8e-a9bf47ba392"> Go To Profile </Link>
+      <Link to="/dashboard/profile/fb1209b1-fabc-4e52-bd8e-a9bf47ba392a"> Go To Profile </Link>
       <Link to="/dashboard"> Go To Root </Link>
     </div>
   )
