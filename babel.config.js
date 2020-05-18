0bd104e078a321bfa4f82a6faf39254c2cf45298
Oleksii <lxvsnk@gmail.com>
2020-05-19 00:25:19 +0800

    update filecopy modules\n fix react-refresh\nadded ssr

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,33 @@
 module.exports = {
-  'presets': [
-    ['@babel/env', {
-      'targets': {
-        'browsers': '> 0.25%, not dead'
-      },
-      'loose': true
-    }],
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: '> 0.25%, not dead'
+        },
+        loose: true
+      }
+    ],
     '@babel/react',
     '@babel/typescript'
   ],
 
-  'plugins': [
+  plugins: (process.env.NODE_ENV === 'development'
+    ? ['react-hot-loader/babel', 'react-refresh/babel']
+    : []
+  ).concat([
     'emotion',
-    'react-hot-loader/babel',
+    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'css-modules-transform',
       {
-        'extensions': ['.styl', '.css', '.scss']
+        extensions: ['.styl', '.css', '.scss']
       }
     ]
-  ]
+  ])
 }
--- client/config/root.js
+++ client/config/root.js
@@ -3,7 +3,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect, StaticRouter } from 'react-router-dom'
 
 import store, { history } from '../redux'
 
@@ -63,10 +63,13 @@ PrivateRoute.propTypes = types
 PrivateRoute.defaultProps = defaults
 OnlyAnonymousRoute.defaultProps = defaults
 
-export default (props) => {
+const ScillcrucialRouter = (props) =>
+  typeof window !== 'undefined' ? <ConnectedRouter {...props} /> : <StaticRouter {...props} />
+
+const Root = (props) => {
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history} location={props.location} context={props.context}>
+      <ScillcrucialRouter history={history} location={props.location} context={props.context}>
         <Startup>
           <Switch>
             <Route exact path="/" component={() => <DummyView />} />
@@ -75,7 +78,9 @@ export default (props) => {
             <Route component={() => <NotFound />} />
           </Switch>
         </Startup>
-      </ConnectedRouter>
+      </ScillcrucialRouter>
     </Provider>
   )
 }
+
+export default Root
