282fb4650adc3f34444bfddbf8f9fafbc655fc49
klekit <klekit72@gmail.com>
2020-06-10 08:53:57 +0300

    added id main

--- client/components/main.js
+++ client/components/main.js
@@ -12,10 +12,11 @@ const Main = () => {
   }
   return (
     <div>
+
       <div>{value}</div>
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-black font-bold rounded-lg border shadow-lg p-10">
-          <input value={value} onChange={onChange} className="mr-3" type="text" />
+          <input id="input-field" value={value} onChange={onChange} className="mr-3" type="text" />
           <button
             className="bg-indigo-300 text-green font-bold rounded-lg shadow-lg p-2"
             type="button"
@@ -25,7 +26,7 @@ const Main = () => {
           </button>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
