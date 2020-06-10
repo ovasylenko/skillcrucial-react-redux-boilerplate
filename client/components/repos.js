fefe22eef5d98d4a3781184e448fd24ba904b7b9
klekit <klekit72@gmail.com>
2020-06-10 17:10:27 +0300

    delete stringify

--- client/components/repos.js
+++ client/components/repos.js
@@ -5,7 +5,6 @@ const Repos = (props) => {
   const { username } = useParams()
   return (
     <div>
-      <div>{JSON.stringify(props)}</div>
       {props.repos.map((repo) => (
         <div className="bg-indigo-600 text-white rounded p-2 mb-2" key={repo.id}>
           <Link to={`/${username}/${repo.name}`}>{repo.name}</Link>
