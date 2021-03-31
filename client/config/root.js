91722f5cf046cb575d0f7f1f9a530bc926bfabbc
Oleksii <lxvslnk@gmail.com>
2021-03-31 21:43:02 +0800

    boilerplate update

--- client/config/root.js
+++ client/config/root.js
@@ -1,7 +1,5 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Provider } from 'react-redux'
+import { Provider, useSelector } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { Switch, Route, Redirect, StaticRouter } from 'react-router-dom'
 
@@ -14,54 +12,32 @@ import NotFound from '../components/404'
 import Startup from './startup'
 
 const OnlyAnonymousRoute = ({ component: Component, ...rest }) => {
-  const func = (props) =>
-    !!rest.user && !!rest.user.name && !!rest.token ? (
-      <Redirect to={{ pathname: '/' }} />
-    ) : (
-      <Component {...props} />
-    )
+  const user = useSelector((state) => state.auth.user)
+  const token = useSelector((state) => state.token)
+  const func = (props) => {
+    if (!!user && !!user.name && !!token) <Redirect to={{ pathname: '/' }} />
+    return <Component {...props} />
+  }
   return <Route {...rest} render={func} />
 }
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const func = (props) =>
-    !!rest.user && !!rest.user.name && !!rest.token ? (
-      <Component {...props} />
-    ) : (
+  const user = useSelector((state) => state.auth.user)
+  const token = useSelector((state) => state.token)
+
+  const func = (props) => {
+    if (!!user && !!user.name && !!token) return <Component {...props} />
+
+    return (
       <Redirect
         to={{
           pathname: '/login'
         }}
       />
     )
+  }
   return <Route {...rest} render={func} />
 }
-
-const types = {
-  component: PropTypes.func.isRequired,
-  location: PropTypes.shape({
-    pathname: PropTypes.string
-  }),
-  user: PropTypes.shape({
-    name: PropTypes.string,
-    email: PropTypes.string
-  }),
-  token: PropTypes.string
-}
-
-const defaults = {
-  location: {
-    pathname: ''
-  },
-  user: null,
-  token: ''
-}
-
-OnlyAnonymousRoute.propTypes = types
-PrivateRoute.propTypes = types
-
-PrivateRoute.defaultProps = defaults
-OnlyAnonymousRoute.defaultProps = defaults
 
 const RouterSelector = (props) =>
   typeof window !== 'undefined' ? <ConnectedRouter {...props} /> : <StaticRouter {...props} />
@@ -72,10 +48,12 @@ const RootComponent = (props) => {
       <RouterSelector history={history} location={props.location} context={props.context}>
         <Startup>
           <Switch>
-            <Route exact path="/" component={() => <DummyView />} />
-            <Route exact path="/dashboard" component={() => <Home />} />
-            <PrivateRoute exact path="/hidden-route" component={() => <DummyView />} />
-            <Route component={() => <NotFound />} />
+            <Route exact path="/" component={DummyView} />
+            <Route exact path="/dashboard" component={Home} />
+            <PrivateRoute exact path="/hidden-route" component={DummyView} />
+            <OnlyAnonymousRoute exact path="/anonymous-route" component={DummyView} />
+
+            <Route component={NotFound} />
           </Switch>
         </Startup>
       </RouterSelector>
